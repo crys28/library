1534524a67ad60e94408d63b9348f0637975e19e
Crys28 <crys_gaming@yahoo.com>
2022-07-07 20:53:28 +0300

    adding library

--- book_order.php
+++ book_order.php
@@ -16,7 +16,7 @@ $sql = "INSERT INTO `comenzi` (`Nume`,`Prenume`,`Oras`,`CodPostal`,`Email`,`Adre
 ('$nume','$prenume','$oras','$postal','$email','$adresa',current_date(),'$idCarte_Titlu');";
 mysqli_query($conn, $sql);
 
-header("Location: ../library/books.php?order=success")
+header("Location: ../crys-library.herokuapp.com/books.php?order=success")
 
 
 
