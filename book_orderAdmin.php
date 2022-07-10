98d30670e7e0198a6ba20cd3a09bc9961cf40cda
Crys28 <crys_gaming@yahoo.com>
2022-07-10 18:09:10 +0300

    adding library

---
+++ book_orderAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once 'database.php';
+
+$nume = $_POST['nume'];
+$prenume = $_POST['prenume'];
+$oras = $_POST['oras'];
+$postal = $_POST['postal'];
+$email = $_POST['email'];
+$adresa = $_POST['adresa'];
+$idCarte_Titlu = $_POST['idCarte_Titlu'];
+
+//  	$sql_id = "SELECT * FROM carte;"
+
+$sql = "INSERT INTO `comenzi` (`Nume`,`Prenume`,`Oras`,`CodPostal`,`Email`,`Adresa`,`Data_Comanda`,`idCarte_Titlu`) VALUES 
+('$nume','$prenume','$oras','$postal','$email','$adresa',current_date(),'$idCarte_Titlu');";
+mysqli_query($conn, $sql);
+
+header("Location: ../booksAdmin.php?order=success")
+
+
+
+
+
+
+
+?>
