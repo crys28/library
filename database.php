b95b37a74f1ab2ebfe499ec88a3964768b44251c
Crys28 <crys_gaming@yahoo.com>
2022-07-10 12:31:04 +0300

    adding library

--- database.php
+++ database.php
@@ -1,9 +1,14 @@
 <?php
 
-$dbServername = "remotemysql.com";
-$dbUsername = "rL46RIpvTF";
-$dbPassword = "XBSjef7uz3";
-$dbName = "rL46RIpvTF";
+// $dbServername = "remotemysql.com";
+// $dbUsername = "rL46RIpvTF";
+// $dbPassword = "XBSjef7uz3";
+// $dbName = "rL46RIpvTF";
+
+$dbServername = "sql11.freemysqlhosting.net";
+$dbUsername = "sql11505211";
+$dbPassword = "ExjnU8lZfV";
+$dbName = "sql11505211";
 
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
