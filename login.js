b73f276851889b561a3866e0066d3291ea48d311
Crys28 <crys_gaming@yahoo.com>
2022-07-23 18:28:33 +0300

    changed_DB_host

--- login.js
+++ login.js
@@ -1,31 +1,3 @@
-// const user = document.getElementById('user');
-
-// user.addEventListener('click', function User() {
-//  document.getElementById('ord').style.cssText = "color: red";
-// });
-
-// function user(){
-// 	 document.getElementById('ord').style.Display = "red";
-// }
-
-
-// $('#user').click(function() {
-//     $('#ord').addClass('userNone');
-// });
-
-
-function change_css(){
-			setTimeout(function() {document.getElementById("ord").style.display = "none";}, 1000);
-        }
-
-// const myTimeout2 = setTimeout(change_css2, 1000);
-
-// function change_css2(){
-//             document.getElementById("ord").style.display = "all";
-//         }
-// document.querySelector('#user').addEventListener('click', () => {
-//   document.querySelector('#ord').classList.add('userNone');
-// }); 
 
 function loginAdmin(){
 	var user = document.getElementById("username").value;
